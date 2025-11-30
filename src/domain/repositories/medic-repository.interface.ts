bcb48ae81a82be1b0abf514026c4d89f46f8fa52
gabbrieu <gabrielhmendes@hotmail.com>
2025-11-30 16:57:10 -0300

    fix: add prismaWhereInput to findUnique method

--- src/domain/repositories/medic-repository.interface.ts
+++ src/domain/repositories/medic-repository.interface.ts
@@ -1,11 +1,10 @@
 import type { MedicEntity, MedicEntityWithoutPassword } from '#entities/medic.entity';
+import type { Prisma } from '#generated/prisma/client';
 import type { CreateMedicRequestDTO } from '#usecases/medic/create-medic/create-medic.dto';
-
-export type MedicUniqueFilter = Partial<MedicEntityWithoutPassword>;
 
 export abstract class MedicRepository {
   abstract findUnique<T extends boolean | undefined = false>(
-    where: MedicUniqueFilter,
+    where: Prisma.MedicWhereUniqueInput,
     withPassword?: T,
   ): Promise<T extends true ? MedicEntity | undefined : MedicEntityWithoutPassword | undefined>;
 
--- src/infra/db/prisma/medic.repository.ts
+++ src/infra/db/prisma/medic.repository.ts
@@ -2,10 +2,11 @@ import type { Logger } from '#config/logger';
 import type { PrismaClientType } from '#db/prisma/client';
 import type { MedicEntity, MedicEntityWithoutPassword } from '#entities/medic.entity';
 import type { Medic, Prisma } from '#generated/prisma/client';
-import type { MedicRepository, MedicUniqueFilter } from '#repositories/medic-repository.interface';
+import type { MedicRepository } from '#repositories/medic-repository.interface';
 import { AppError } from '#shared/errors/app-error';
 import { HttpStatusCode } from '#shared/http-status-code.enum';
 import type { CreateMedicRequestDTO } from '#usecases/medic/create-medic/create-medic.dto';
+import { DateTime } from 'luxon';
 import { inject, injectable } from 'tsyringe';
 
 @injectable()
@@ -16,12 +17,12 @@ export class PrismaMedicRepository implements MedicRepository {
   ) {}
 
   async findUnique<T extends boolean | undefined = false>(
-    where: MedicUniqueFilter,
+    where: Prisma.MedicWhereUniqueInput,
     withPassword?: T,
   ): Promise<T extends true ? MedicEntity | undefined : MedicEntityWithoutPassword | undefined> {
     try {
       const medic = await this.prisma.medic.findUnique({
-        where: where as Prisma.MedicWhereUniqueInput,
+        where,
       });
 
       if (!medic)
@@ -31,7 +32,7 @@ export class PrismaMedicRepository implements MedicRepository {
 
       return this.mapToDomain(medic, withPassword);
     } catch (error: any) {
-      this.logger.error(error);
+      this.logger.error(error, 'Erro na consulta do repositório de Médicos');
       throw new AppError({
         message: error.message || 'Erro na consulta do repositório de Médicos',
         statusCode: HttpStatusCode.INTERNAL_SERVER_ERROR,
@@ -43,34 +44,42 @@ export class PrismaMedicRepository implements MedicRepository {
     dto: CreateMedicRequestDTO,
     hashedPassword: string,
   ): Promise<MedicEntityWithoutPassword> {
-    const medic = await this.prisma.medic.create({
-      data: {
-        ...dto,
-        password: hashedPassword,
-      },
-    });
+    try {
+      const medic = await this.prisma.medic.create({
+        data: {
+          ...dto,
+          password: hashedPassword,
+        },
+      });
 
-    return this.mapToDomain(medic, false);
+      return this.mapToDomain(medic, false);
+    } catch (error: any) {
+      this.logger.error(error, 'Erro na criação do repositório de Médicos');
+      throw new AppError({
+        message: error.message || 'Erro na criação do repositório de Médicos',
+        statusCode: HttpStatusCode.INTERNAL_SERVER_ERROR,
+      });
+    }
   }
 
   private mapToDomain<T extends boolean | undefined = false>(
-    user: Medic,
+    medic: Medic,
     withPassword?: T,
   ): T extends true ? MedicEntity | undefined : MedicEntityWithoutPassword {
-    const userBase: MedicEntityWithoutPassword = {
-      id: user.id,
-      email: user.email,
-      createdAt: user.createdAt,
-      updatedAt: user.updatedAt,
+    const medicBase: MedicEntityWithoutPassword = {
+      id: medic.id,
+      email: medic.email,
+      createdAt: DateTime.fromJSDate(medic.createdAt),
+      updatedAt: DateTime.fromJSDate(medic.updatedAt),
     };
 
     if (withPassword) {
       return {
-        ...userBase,
-        password: user.password,
+        ...medicBase,
+        password: medic.password,
       } as T extends true ? MedicEntity : never;
     }
 
-    return userBase as T extends true ? never : MedicEntityWithoutPassword;
+    return medicBase as T extends true ? never : MedicEntityWithoutPassword;
   }
 }
